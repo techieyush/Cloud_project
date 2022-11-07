38e0a657f8a537dc86a03f8c736adcffa5675023
Ayush Yadav <ayushcs.net24@gmail.com>
2022-11-07 19:59:56 +0530

    Local Changes

---
+++ script.js
@@ -0,0 +1,15 @@
+
+function newTab1()
+{
+    window.open("page1.html","_blank");
+}
+function newTab2()
+{
+    window.open("page2.html","_blank");
+}
+function SignInForm(){
+    window.open('SignIn.html','_blank');
+}
+function RegisterForm(){
+    window.open('RegisterForm.html','_blank');
+}
